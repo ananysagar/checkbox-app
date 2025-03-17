3cc4175b0013b87da6f6b60de5994a21e4f3a6ab
ananysagar <sagaranany577@gmail.com>
2025-03-17 23:06:52 +0530

    selectAll checkbox logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { toppings } from "./toppings";
 
 function App() {
+  const [allChecked, setAllChecked] = useState(false)
+  const [checkedList, setCheckedList] = useState([])
+
+  const handleChange = (e) => {
+    const {name, checked} = e.target;
+    if(checked) {
+      setCheckedList((prev) => [...prev, name])
+    } else {
+      setCheckedList((prev) => prev.filter((item) => item!== name))
+    }
+  }
+
+  const handleSelectAll = (e) => {
+    const isChecked = e.target.checked;
+    setAllChecked(isChecked)
+
+    if(isChecked) {
+      setCheckedList(toppings.map((item) => item.name))
+    } else {
+      setCheckedList([])
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Toppings</h1>
+      <div className="checkbox-app">
+        <div className="toppings-list">
+          <input type="checkbox" id="selectAll" onChange={handleSelectAll} checked={allChecked}/>
+          <label htmlFor="selectAll">Select All</label>
+          {toppings.map((element, name) => {
+            return (
+              <div className="list-items" key={element.name}>
+                <input type="checkbox" name={element.name} onChange={handleChange} checked={checkedList.includes(element.name)}/>
+                <div>{element.name}</div>
+                <div><span>:  Rs </span>{element.price}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
