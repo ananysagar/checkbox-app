8553ebcd8ec51f1e6ef88405a5b655b972fb8570
ananysagar <sagaranany577@gmail.com>
2025-03-18 22:05:37 +0530

    format check

--- src/App.js
+++ src/App.js
@@ -3,42 +3,55 @@ import "./App.css";
 import { toppings } from "./toppings";
 
 function App() {
-  const [allChecked, setAllChecked] = useState(false)
-  const [checkedList, setCheckedList] = useState([])
+  const [allChecked, setAllChecked] = useState(false);
+  const [checkedList, setCheckedList] = useState([]);
 
   const handleChange = (e) => {
-    const {name, checked} = e.target;
-    if(checked) {
-      setCheckedList((prev) => [...prev, name])
+    const { name, checked } = e.target;
+    if (checked) {
+      setCheckedList((prev) => [...prev, name]);
     } else {
-      setCheckedList((prev) => prev.filter((item) => item!== name))
+      setCheckedList((prev) => prev.filter((item) => item !== name));
     }
-  }
+  };
 
   const handleSelectAll = (e) => {
     const isChecked = e.target.checked;
-    setAllChecked(isChecked)
+    setAllChecked(isChecked);
 
-    if(isChecked) {
-      setCheckedList(toppings.map((item) => item.name))
+    if (isChecked) {
+      setCheckedList(toppings.map((item) => item.name));
     } else {
-      setCheckedList([])
+      setCheckedList([]);
     }
-  }
+  };
 
   return (
     <div className="App">
       <h1>Select Toppings</h1>
       <div className="checkbox-app">
         <div className="toppings-list">
-          <input type="checkbox" id="selectAll" onChange={handleSelectAll} checked={allChecked}/>
+          <input
+            type="checkbox"
+            id="selectAll"
+            onChange={handleSelectAll}
+            checked={allChecked}
+          />
           <label htmlFor="selectAll">Select All</label>
           {toppings.map((element, name) => {
             return (
               <div className="list-items" key={element.name}>
-                <input type="checkbox" name={element.name} onChange={handleChange} checked={checkedList.includes(element.name)}/>
+                <input
+                  type="checkbox"
+                  name={element.name}
+                  onChange={handleChange}
+                  checked={checkedList.includes(element.name)}
+                />
                 <div>{element.name}</div>
-                <div><span>:  Rs </span>{element.price}</div>
+                <div>
+                  <span>: Rs </span>
+                  {element.price}
+                </div>
               </div>
             );
           })}
